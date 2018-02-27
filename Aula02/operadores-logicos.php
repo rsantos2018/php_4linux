f4b10a952fcd01994d3b0625ff8de2b78a1a325c
Reginaldo dos Santos <regi35@terra.com.br>
2018-02-27 20:48:11 -0300

    atualizacao 3

---
+++ Aula02/operadores-logicos.php
@@ -0,0 +1,24 @@
+<?php
+
+echo "<pre>";
+
+var_dump(true and true); //true
+var_dump(true and false); //false
+var_dump(true && true); //true
+var_dump(true && false); //false
+echo "<hr>";
+
+var_dump(true or true); //true
+var_dump(true or false); //false
+var_dump(true || true); //true
+var_dump(true || false); //true
+var_dump(false || false); //false
+echo "<hr>";
+
+var_dump(true xor true); //false
+var_dump(true xor false); //true
+var_dump(false xor false); //true
+echo "<hr>";
+
+var_dump(!true); //false
+var_dump(!false); //true
