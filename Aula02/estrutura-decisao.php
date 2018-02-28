4ce6dd5ed254d1925413b9d888e263aa2bf54011
Reginaldo dos Santos <regi35@terra.com.br>
2018-02-27 22:03:13 -0300

    atualizacao 8

--- Aula02/estrutura-decisao.php
+++ Aula02/estrutura-decisao.php
@@ -12,5 +12,22 @@ if (($idade >= 18) && $carterinha){
 else if (($idade >=18) && $carterinha == false){
 	echo "Maior idade, faça carterinha";
 }else {
-	echo "Menor de idade, carterinha não permitida.";
+	echo "Menor idade, carterinha não permitida";
 }
+	
+
+echo "<hr>";
+
+
+$idade = 61;
+
+if ($idade >=18 && $idade < 60){
+	echo "Voto obrigatório!";
+} elseif ($idade >=16 || $idade >= 60) {
+	echo "Voto opcional!";
+}elseif ($idade < 16) {
+		echo "Não vota!";
+}else {
+	echo "Idade Inválida!";
+}
+
---
+++ Aula02/exercicio 1-aula02.php
@@ -0,0 +1,9 @@
+<?php
+
+$num = 3;
+
+if($num % 2 === 0){
+	echo "O número é par.";
+} else {
+	echo " O número é impar.";
+}
