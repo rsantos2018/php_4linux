f7d33bff52955abe5c8796b15f43563293ff30a2
Reginaldo dos Santos <regi35@terra.com.br>
2018-02-27 22:15:07 -0300

    atualizacao 9

---
+++ Aula02/estrutura-decisao-switch.php
@@ -0,0 +1,20 @@
+<?php
+
+$nome = readline("Digite seu nome: ");
+
+
+switch (variable) {
+	case 'value':
+		# code...
+		break;case 'Reginaldo':
+		echo "Boa noite, Reginaldo"
+		break;
+
+	case 'Lucas':
+		echo "Boa noite, Lucas"
+		break;
+
+	default:
+		echo "Boa noite, Estranho!!!"
+		break;
+}
