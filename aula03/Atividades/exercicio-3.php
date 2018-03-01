af29033532edb9f50ceeb0a55706fcd0c4d66709
Reginaldo dos Santos <regi35@terra.com.br>
2018-02-28 22:20:15 -0300

    exerciciosatual-28-02-18

--- aula03/Atividades/exercicio-3.php
+++ aula03/Atividades/exercicio-3.php
@@ -25,3 +25,12 @@ echo "<hr>";
 
 var_dump($num1 <= $num2);
 echo "<hr>";
+echo "<br>";
+
+$a = true;
+$b = false;
+
+var_dump($a and $b);
+var_dump($a && $b);
+var_dump($a or $b);
+var_dump($a || $b);
